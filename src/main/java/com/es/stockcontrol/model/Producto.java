848b6ff4fb166ecc7481f2a5758980a86aeb175b
frademuca <fmuncar193@g.educaand.es>
2024-10-18 19:14:50 +0200

    Paquete model

--- src/main/java/com/es/stockcontrol/model/Producto.java
+++ src/main/java/com/es/stockcontrol/model/Producto.java
@@ -1,4 +1,83 @@
 package com.es.stockcontrol.model;
 
+import java.util.Date;
+
 public class Producto {
+
+    private String id;
+    private String categoria;
+    private String nombre;
+    private String descripcion;
+    private float precio_sin_iva;
+    private float precio_con_iva;
+    private Date fecha_alta;
+    private int stock;
+
+
+    public Producto() {}
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio_sin_iva() {
+        return precio_sin_iva;
+    }
+
+    public void setPrecio_sin_iva(float precio_sin_iva) {
+        this.precio_sin_iva = precio_sin_iva;
+    }
+
+    public float getPrecio_con_iva() {
+        return precio_con_iva;
+    }
+
+    public void setPrecio_con_iva(float precio_con_iva) {
+        this.precio_con_iva = precio_con_iva;
+    }
+
+    public Date getFecha_alta() {
+        return fecha_alta;
+    }
+
+    public void setFecha_alta(Date fecha_alta) {
+        this.fecha_alta = fecha_alta;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
--- src/main/java/com/es/stockcontrol/model/User.java
+++ src/main/java/com/es/stockcontrol/model/User.java
@@ -1,4 +1,27 @@
 package com.es.stockcontrol.model;
 
 public class User {
+
+    private String nombre_usuario;
+    private String password;
+
+
+    public User(){}
+
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
