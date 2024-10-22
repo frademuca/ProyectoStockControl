e7b7f78be499ad1462418ac21cd50c6801607f5c
frademuca <fmuncar193@g.educaand.es>
2024-10-22 21:30:19 +0200

    User_Service

---
+++ src/main/java/com/es/stockcontrol/service/UserService.java
@@ -0,0 +1,31 @@
+package com.es.stockcontrol.service;
+
+import com.es.stockcontrol.model.User;
+import com.es.stockcontrol.repository.UserRepository;
+
+import static java.util.Optional.empty;
+
+public class UserService {
+
+    public boolean login(String username, String password) {
+
+        username = empty();
+
+        // 1º Obtener el usuario de la base de datos
+        User user = new User();
+        UserRepository userRepository = new UserRepository();
+        user = userRepository.read(username);
+
+        // 2º Comprobar que el username y la password coinciden con el obtenido de la bd
+        if (user == null){
+            return false;
+        }
+
+        if (user.getNombreUsuario().equals(username) && user.getPassword().equals(password)){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
